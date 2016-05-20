1043b1475369f267b7d5f82f92e071cb48c73389
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-05-20 07:42:38 +0100

    Add simple cell component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Config from './config.json';
+import Cell from './components/Cell';
+
+let App = React.createClass({
+    render: function () {
+        return <div>
+            <Cell alive="true" />
+        </div>;
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
